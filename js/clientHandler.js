04c419624274c4c5f58c8fd9ab448272c3ad66b5
Akshay mishra <akshayetw@gmail.com>
2018-02-27 11:15:34 +0530

    hj
    Signed-off-by: Akshay mishra <akshayetw@gmail.com>

--- js/clientHandler.js
+++ js/clientHandler.js
@@ -1,37 +1,58 @@
-var initializePush = function() {
+var App42SafariPushService = {};
+
+(function () {
+    this.webPushId;
+    this.apiKey;
+    this.secretKey;
+    this.initialize = function () {
+        if (this.webPushId && this.apiKey) {
+            initializePush(webPushId);
+        } else {
+            console.warn("WEB-PUSH-ID / API-KEY IS NULL OR BLANK.")
+        }
+    }
+}).apply(App42SafariPushService);
+
+
+var initializePush = function () {
+    console.log("Unsupported App42SafariPushService.web " + App42SafariPushService.webPushId);
+    console.log("Unsupported App42SafariPushService.apiKey " + App42SafariPushService.apiKey);
     // Ensure that the user can receive Safari Push Notifications.
     if ('safari' in window && 'pushNotification' in window.safari) {
-        var webPushId = 'web.com.shephertz.safariakm';
-        var permissionData = window.safari.pushNotification.permission(webPushId);
+        // var webPushId = 'web.com.shephertz.safariakm';
+        var permissionData = window.safari.pushNotification.permission(App42SafariPushService.webPushId);
         checkRemotePermission(permissionData);
-    }else{
+    } else {
         console.log("Unsupported device.");
-        alert ("Unsupported device.");
+        // alert ("Unsupported device.");
     }
 };
 
 var checkRemotePermission = function (permissionData) {
-    var webPushId = "web.com.shephertz.safariakm";
-    var getPushPackageUrl = "https://safaripush.shephertz.com";
-    console.log("11111111",permissionData);
+    //var webPushId = "web.com.shephertz.safariakm";
+    var webServiceUrl = "https://safaripush.shephertz.com";
+    console.log("1 ", permissionData);
     if (permissionData.permission === 'default') {
-        console.log("22222",permissionData);
+        //Notification.
+        console.log("2 ", permissionData);
         // This is a new web service URL and its validity is unknown.
         window.safari.pushNotification.requestPermission(
-            getPushPackageUrl, // The web service URL.
-            webPushId,     // The Website Push ID.
-            {"apiKey":"07615a186d1377718979b0d92c146e750ced1c90d62aa8cbd340fb79a04b330f"}, // Data that you choose to send to your server to help you identify the user.
+            webServiceUrl, // The web service URL.
+            App42SafariPushService.webPushId,     // The Website Push ID.
+            { "apiKey": App42SafariPushService.apiKey,
+            "secretKey": App42SafariPushService.secretKey
+         }, // Data that you choose to send to your server to help you identify the user.
             checkRemotePermission         // The callback function.
         );
     }
     else if (permissionData.permission === 'denied') {
-        console.log("3333",permissionData);
+        console.log("3 ", permissionData);
         // The user said no.
     }
     else if (permissionData.permission === 'granted') {
         // The web service URL is a valid push provider, and the user said yes.
         // permissionData.deviceToken is now available to use.
-        console.log("permissionData ", permissionData);
-        alert("Device Token Recieved : " + permissionData.deviceToken);
+        console.log("4 ", permissionData);
+        //alert("Device Token Recieved : " + permissionData.deviceToken);
     }
 };
